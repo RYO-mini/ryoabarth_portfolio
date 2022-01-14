f8aec3b678113261599243cbc70c35913c11eeda
RyotaKomatsu <komatsu@ryo-mini.net>
2022-01-14 08:57:53 +0000

    Basic認証を導入

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth.basic');
